c524d8f3c6a5b6e5335c07c5e2f9d1b5ebea9df8
Hoang Minh Nguyen <60809722+minh220399@users.noreply.github.com>
2023-07-19 17:46:59 -0400

    Update name for index file

--- server/myscript.js
+++ server/myscript.js
@@ -32,35 +32,6 @@ app.get("/videos", (req, res) =>{
     });
 });
 
-// app.get("/videosByGame/:game", (req, res) =>{
-//     var game = req.params.game;
-//     console.log("GET /videosByGame/" + game);
-
-//     connection.query(" SELECT * FROM ChuaKa.video_info where game = '" + game + "'", function (err, result,fields) {
-//         if (err)
-//         {
-//             console.error("fail to query" + err.stack);
-//                 return;
-//         }
-//         var length = Object.keys(result).length;
-//         var respone = [] ;
-
-//         for (var i = 0; i < length; i++)
-//         {
-//             var obj = {
-//                 title: result[i].title,
-//                 link: result[i].link,
-//                 game: result[i].game,
-//                 pov: result
-//             };
-//             respone.push(obj);
-            
-//         }
-//         res.json(respone);
-
-//     });
-// });
-
 app.listen(8080, () => {
     console.log("listening on port 8080");
 });
